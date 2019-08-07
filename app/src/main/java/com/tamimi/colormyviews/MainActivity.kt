4cb87c4a832891364994b5b932cb0093817e6b62
YASSER ALTAMIMI <yosy2010@gmail.com>
2019-08-08 01:38:19 +0300

    finished lesson

--- app/src/main/java/com/tamimi/colormyviews/MainActivity.kt
+++ app/src/main/java/com/tamimi/colormyviews/MainActivity.kt
@@ -15,7 +15,38 @@ class MainActivity : AppCompatActivity() {
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_five_text -> view.setBackgroundColor(Color.RED)
+            R.id.box_six_text -> view.setBackgroundColor(Color.BLACK)
+            R.id.box_seven_text -> view.setBackgroundColor(Color.CYAN)
+            R.id.box_eight_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_nine_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_ten_text -> view.setBackgroundColor(Color.RED)
+
+            R.id.red_button -> {
+                box_one_text.setBackgroundResource(R.color.my_red)
+                box_three_text.setBackgroundResource(R.color.my_red)
+                box_five_text.setBackgroundResource(R.color.my_red)
+                box_ten_text.setBackgroundResource(R.color.my_red)
+            }
+
+
+            R.id.yellow_button -> {
+                box_two_text.setBackgroundResource(R.color.my_yellow)
+                box_four_text.setBackgroundResource(R.color.my_yellow)
+                box_six_text.setBackgroundResource(R.color.my_yellow)
+                box_seven_text.setBackgroundResource(R.color.my_yellow)
+            }
+
+
+            R.id.green_button -> {
+                box_three_text.setBackgroundResource(R.color.my_green)
+                box_eight_text.setBackgroundResource(R.color.my_green)
+                box_ten_text.setBackgroundResource(R.color.my_green)
+                box_one_text.setBackgroundResource(R.color.my_green)
+            }
+
+
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -29,7 +60,15 @@ class MainActivity : AppCompatActivity() {
             box_three_text,
             box_four_text,
             box_five_text,
-            constrain_layout
+            box_six_text,
+            box_seven_text,
+            box_eight_text,
+            box_nine_text,
+            box_ten_text,
+            constrain_layout,
+            red_button,
+            green_button,
+            yellow_button
         )
 
         clickableViews.forEach { view ->
